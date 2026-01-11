eed7fbf4a5d184dc74e7bdaa5a8eaee8a4b5475f
DanRCM <rcortezmanzano24@gmail.com>
2026-01-10 21:30:00 -0500

    Personalizacion de app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'temp',
-  webDir: 'dist'
+  appId: 'com.espolmarket.app',
+  appName: 'EspolMarket',
+  webDir: 'dist',
 };
 
 export default config;
